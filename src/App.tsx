4b14cc6c0c0c7873154dceee07d2ce376bf8e355
catrius <catrius@linecorp.com>
2025-10-20 03:08:52 +0700

    feat: No auto scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-import { Outlet, useLocation } from 'react-router';
+import { Outlet } from 'react-router';
 import Nav from '@/components/Nav';
 import Footer from '@/components/Footer.tsx';
-import { useEffect } from 'react';
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location.pathname]);
-
   return (
     // flex h-screen flex-col mb-auto: This combo makes footer sits at bottom
     <div className="flex h-screen min-w-93 flex-col">
